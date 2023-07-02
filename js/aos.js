ea32768ce6a40da30d2348b319d5696a94106065
maryan <smoliymaryan89@gmail.com>
2023-07-02 13:13:22 +0300

    added mobile-menu

--- js/aos.js
+++ js/aos.js
@@ -1,5 +1,19 @@
-AOS.init({
-  duration: 700,
-  easing: 'ease-in-out',
-  once: true,
+window.addEventListener('load', function () {
+  function toggleAOS() {
+    if (window.innerWidth > 768) {
+      AOS.init({
+        duration: 700,
+        easing: 'ease-in-out',
+        once: true,
+      });
+    } else {
+      AOS.init({
+        disable: true,
+      });
+    }
+  }
+
+  toggleAOS();
+
+  window.addEventListener('resize', toggleAOS);
 });
